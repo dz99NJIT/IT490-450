6a669bb8585ef14585fe463b28029b1fd9be823c
dz99NJIT <60232027+dz99NJIT@users.noreply.github.com>
2020-06-17 18:11:29 -0400

    Create functions.php

---
+++ APP/functions.php
@@ -0,0 +1,71 @@
+<?php
+
+//Required files
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+require_once('AppRabbitMQClient.php');
+
+//Start session
+//session_start();
+
+	//Check if user is logged in, redirect to login page if not
+	function loggedCheck(){
+		if (!$_SESSION["logged"]){
+			header("Location: index.html");
+		}
+	}
+
+	//Login function
+    function login($uname, $pw){
+        
+        $request = array();
+        
+        $request['type'] = "Login";
+        $request['uname'] = $uname;
+        $request['pw'] = $pw;
+
+        $returnedValue = createClientForDb($request);
+        
+        if($returnedValue == 1){
+            $_SESSION["uname"] = $uname;
+            $_SESSION["logged"] = true;
+			header("Location: landingPage.php");
+			return true;
+        }else{
+			header("Location: index.html");
+            session_destroy();
+			return false;
+        }
+        return $returnedValue;
+    }
+
+    //Sign up function
+    function SignUp($Fullname, $uname, $pw){
+        
+        $request = array();
+        
+        $request['type'] = "SignUp";
+	$request['Fullname'] = $Fullname;
+        $request['uname'] = $uname;
+        $request['pw'] = $pw;
+
+        $returnedValue = createClientForDb($request);
+
+        return $returnedValue;
+    }
+
+    //Search user function
+    function searchUser($friend){
+        
+        $request = array();
+        
+        $request['type'] = "UserSearch";
+        $request['friend'] = $friend;
+
+        $returnedValue = createClientForDb($request);
+		
+        return $returnedValue;
+    }
+
+?>
