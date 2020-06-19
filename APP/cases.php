1e52104c621a9ea8fd9735271768fc8324690283
ELVIS A RODRIGUEZ <40173726+elvisr21@users.noreply.github.com>
2020-06-19 14:16:28 -0400

    commit

--- APP/cases.php
+++ APP/cases.php
@@ -8,6 +8,12 @@ require_once('AppRabbitMQClient.php');
 include("functions.php");
 include("landingPage.php");
 
+//enables error messages
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 session_start();
 echo "<script>console.log('pass1');</script>";
 
@@ -15,33 +21,33 @@ $type = $_POST["type"];
 echo "<script>console.log('pass2 ');</script>";
 
 switch ($type){
-  
+
   //Login Case
   case "Login":
-  
+
     $uname = $_POST["uname"];
     $pw = $_POST["pw"];
-    
+
     $response = login($uname, $pw);
-    
+
     if (!$response){
-     	echo $response; 
+     	echo $response;
 		echo '<div class="alert alert-danger" role="alert">Incorrect login, please try again!</div>';
 		}
 		else{
-			echo $response; 
+			echo $response;
 		}
-    break;  
+    break;
  echo "<script>console.log('pass3 ');</script>";
-  
+
   //Sign up Case
   case "SignUp":
-  
+
     $Fullname = $_POST["Fullname"];
     $uname = $_POST["uname"];
     $pw = $_POST["pw"];
 	$confirmPw = $_POST["confirmPw"];
-		
+
 	if ($confirmPw!=$pw){
         return '<div class="alert alert-danger" role="alert">Passwords do not match, try again</div>';
 	}
@@ -51,8 +57,8 @@ switch ($type){
 			return '<div class="alert alert-success" role="alert">Successfully created your account, please login to the left!</div>';}
 	}
 	break;
-		
+
   default:
     return "Default Case";
-}    
+}
 ?>
