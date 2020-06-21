5c900246557ff5ad2707bff2ac0ddffef3b2a824
root <root@ip-172-31-34-246.ec2.internal>
2020-06-21 03:47:33 +0000

    added log function

--- DB/dbListener.php
+++ DB/dbListener.php
@@ -24,14 +24,19 @@
         echo "received request".PHP_EOL;
         echo $request['type'];
         var_dump($request);
-        
+	
+	logger($request);
+
         if(!isset($request['type'])){
             return array('message'=>"ERROR: Message type is not supported");
         }
         
         $type = $request['type'];
-        switch($type){
-                
+	
+	logger($type);
+
+	switch($type){
+		
             //Login & Authentication request    
             case "Login":
                 echo "<br>login";
@@ -49,13 +54,18 @@
                 break;
         }
         
-        echo $response_msg;
-        return $response_msg;        
+	echo $response_msg;
+
+	logger($response_msg);
+	
+	return $response_msg;        
     }        
 
     //Creates new rabbit server
     $server = new rabbitMQServer('rabbitMQ_db.ini', 'testServer');
-    
+
+    logger($server);
+
     //processes requests sent by client
     $server->process_requests('requestProcessor');
         
