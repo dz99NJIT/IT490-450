a715077d4f2cd2e695b64958be168659fa00be0e
jj356 <41375860+jj356@users.noreply.github.com>
2020-06-21 04:23:57 -0400

    Update dbConnect.php
    changed user login to original login

--- DB/dbConnect.php
+++ DB/dbConnect.php
@@ -3,7 +3,7 @@
     //Connects to database
     function dbConnect(){
         
-      $mydb = new mysqli('127.0.0.1','it490admin','dbPassword','LetM3in!');
+      $mydb = new mysqli('127.0.0.1','prodDb','dbPassword','SportWatch');
       
       if ($mydb->errno != 0)
       {
