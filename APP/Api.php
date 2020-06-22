d86ecc6a3f52cae185b42c0ddf43c3064a73180b
ELVIS A RODRIGUEZ <40173726+elvisr21@users.noreply.github.com>
2020-06-22 19:41:13 -0400

    api call

--- APP/Api.php
+++ APP/Api.php
@@ -4,8 +4,11 @@
 $url= "http://api.sportradar.us/draft/nba/trial/v1/en/2019/prospects.json?api_key=nyfttfgabf7hq86m79kmxjy4";
 
 //call api
+    // gets json frm api
 $json = file_get_contents($url);
+    //decodes the data
 $json = json_decode($json);
+    //gets data from decoded data
 $result = $json->draft->id;
 echo "hello<br>";
 echo "results: ";
