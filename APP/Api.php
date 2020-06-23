ce4257ecdea9d17b8034f4af6533c7c6cb484473
ELVIS A RODRIGUEZ <40173726+elvisr21@users.noreply.github.com>
2020-06-23 15:16:06 -0400

    setting up landpage.js function for later use. and setting up api information

--- APP/Api.php
+++ APP/Api.php
@@ -7,11 +7,11 @@ $url= "http://api.sportradar.us/draft/nba/trial/v1/en/2019/prospects.json?api_ke
     // gets json frm api
 $json = file_get_contents($url);
     //decodes the data
-$json = json_decode($json);
+//$json = json_decode($json);
     //gets data from decoded data
-$result = $json->draft->id;
-echo "hello<br>";
-echo "results: ";
-echo $result;
-echo "<br>"
+$result = $json->draft;
+
+//echo json_encode($result);
+//echo json_encode($json);
+echo $json;
 ?>
--- APP/landingPage.js
+++ APP/landingPage.js
@@ -1,9 +1,39 @@
-window.onload=function(){
+function update(){
     var http=new XMLHttpRequest();
+    var test=document.getElementById("chat");
     http.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("test").innerHTML = this.responseText;
+            if(this.responseText!=""){
+                test.innerHTML = test.innerHTML + this.responseText;
+            }
         }
     };
-    xhttp.open("GET","Api.php",true)
+    http.open("GET","Api.php",true);
+    http.send();
+}
+function APIcall(){
+    var http=new XMLHttpRequest();
+    var test=document.getElementById("test");
+    http.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            //var j = this.responseText;
+            //var j = JSON.parse(this.responseText);
+            alert(this.responseText);
+            alert(j);
+            alert(j.draft.id);
+        }
+    };
+    http.open("GET","Api.php",true);
+    http.send();
+}
+function interval(){
+    setInterval(function(){
+        APIcall();
+        update();
+    }, 5000);
+
+}
+window.onload=function(){
+    update();
+    interval();
 }
