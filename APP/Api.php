39c614dac5aad0782d3c6f2831decf31c6b0ece1
ELVIS A RODRIGUEZ <40173726+elvisr21@users.noreply.github.com>
2020-06-23 20:29:49 -0400

    commit

--- APP/Api.php
+++ APP/Api.php
@@ -1,26 +1,53 @@
 <?php
 
 //url for api call
-$url= "http://api.sportradar.us/draft/nba/trial/v1/en/2019/prospects.json?api_key=nyfttfgabf7hq86m79kmxjy4";
-       http://api.sportradar.us/draft/nfl/trial/v1/en/2019/prospects.json?api_key=
+    //first one if to get team
+    //second is to get player
+    //third one is for getting player stats
 $url=array(
-    //for getting team id
-    "http://api.sportradar.us/draft/nfl/trial/v1/en/2019/prospects.json?api_key=",
-    //for getting team id
-    "http://api.sportradar.us/nfl/official/trial/v5/en/teams/97354895-8c77-4fd4-a860-32e62ea7382a/profile.json?api_key=",
-    //for getting player stats
-    "http://api.sportradar.us/nfl/official/trial/v5/en/players/41c44740-d0f6-44ab-8347-3b5d515e5ecf/profile.json?api_key=",
-
+        "nfl"=>array(
+            "http://api.sportradar.us/draft/nfl/trial/v1/en/2019/prospects.json?api_key=wdyztaakwe8sny3vb4prt863",
+            "http://api.sportradar.us/nfl/official/trial/v5/en/teams/97354895-8c77-4fd4-a860-32e62ea7382a/profile.json?api_key=wdyztaakwe8sny3vb4prt863",
+            "http://api.sportradar.us/nfl/official/trial/v5/en/players/41c44740-d0f6-44ab-8347-3b5d515e5ecf/profile.json?api_key=wdyztaakwe8sny3vb4prt863"),
+        //has a updated attribute
+        "nba"=>array(
+            "http://api.sportradar.us/draft/nba/trial/v1/en/2019/prospects.json?api_key=nyfttfgabf7hq86m79kmxjy4",
+            "http://api.sportradar.us/nba/trial/v7/en/teams/583ec825-fb46-11e1-82cb-f4ce4684ea4c/profile.json?api_key=nyfttfgabf7hq86m79kmxjy",
+            "http://api.sportradar.us/nba/trial/v7/en/players/ab532a66-9314-4d57-ade7-bb54a70c65ad/profile.json?api_key=nyfttfgabf7hq86m79kmxjy4"),
+        "lol"=>array(
+            //first one is for getting tournament id
+            //second one is for getting competitors id aka team id
+            //third for team profile to get player id
+            //fourth for player stats
+            "http://api.sportradar.us/lol-t1/en/tournaments.json?api_key=fws7zff8ytnyzmugw6ba8vxc",
+            "http://api.sportradar.us/lol-t1/en/tournaments/sr:tournament:2450/summaries.json?api_key=fws7zff8ytnyzmugw6ba8vxc",
+            "http://api.sportradar.us/lol-t1/en/teams/sr:competitor:240582/profile.json?api_key=fws7zff8ytnyzmugw6ba8vxc",
+            "http://api.sportradar.us/lol-t1/en/players/sr:player:949022/profile.json?api_key=fws7zff8ytnyzmugw6ba8vxc"),
+        "csgo"=>array(
+            "http://api.sportradar.us/csgo-t1/en/tournaments.json?api_key=rxnve45j95ac742auc5xt329",
+            "http://api.sportradar.us/csgo-t1/en/tournaments/sr:tournament:2390/summaries.json?api_key=rxnve45j95ac742auc5xt329",
+            "http://api.sportradar.us/csgo-t1/en/teams/sr:competitor:220602/profile.json?api_key=rxnve45j95ac742auc5xt329",
+            "http://api.sportradar.us/csgo-t1/en/players/sr:player:917768/profile.json?api_key=rxnve45j95ac742auc5xt329"),
+        "dota2"=>array(
+            "http://api.sportradar.us/dota2-t1/en/tournaments.json?api_key=tvkmchnyurfvyez8jzv4hma4",
+            "http://api.sportradar.us/dota2-t1/en/tournaments/sr:tournament:2390/summaries.json?api_key=tvkmchnyurfvyez8jzv4hma4",
+            "http://api.sportradar.us/dota2-t1/en/teams/sr:competitor:220602/profile.json?api_key=tvkmchnyurfvyez8jzv4hma4",
+            "http://api.sportradar.us/dota2-t1/en/players/sr:player:917768/profile.json?api_key=tvkmchnyurfvyez8jzv4hma4")
+        )
 );
 $keys = array(
-  "nba"=>"nyfttfgabf7hq86m79kmxjy4",
-  "lol"=> "fws7zff8ytnyzmugw6ba8vxc",
-  "dota2"=>"tvkmchnyurfvyez8jzv4hma4",
-  "mlb"=> "2ftdna96n3yx6q9rkekbrbbc",
-  "nfl"=> "wdyztaakwe8sny3vb4prt863",
-  "ufc"=> "yyutm7yrpzbexfehgaeqj289",
-  "csgo"=> "rxnve45j95ac742auc5xt329"
+  "nba"=>"nyfttfgabf7hq86m79kmxjy4", //done
+  "lol"=> "fws7zff8ytnyzmugw6ba8vxc", // done
+  "dota2"=>"tvkmchnyurfvyez8jzv4hma4", //done
+  "mlb"=> "2ftdna96n3yx6q9rkekbrbbc", // cant be used
+  "nfl"=> "wdyztaakwe8sny3vb4prt863", // done
+  "ufc"=> "yyutm7yrpzbexfehgaeqj289", //cant be used
+  "csgo"=> "rxnve45j95ac742auc5xt329" //done
 );
+$version = array (
+  "nfl"=>"v5"
+  "nba"=>
+)
 $Sports=array("LOL","DOTA2","MLB","NFL","UFC","NBA","CSGO");
 //call api
     // gets json frm api
