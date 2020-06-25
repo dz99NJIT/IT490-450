1641b2cce7a2af89c5bff24b71610cb8819ff01c
dz99NJIT <60232027+dz99NJIT@users.noreply.github.com>
2020-06-25 16:38:19 -0400

    Update landingPage.php
    commenting out landingpage.js

--- APP/landingPage.php
+++ APP/landingPage.php
@@ -10,7 +10,7 @@ require_once('functions.php');
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-	<script src=landingPage.js></script>
+<!--	<script src=landingPage.js></script>     -->
 </head>
 <body>
 
