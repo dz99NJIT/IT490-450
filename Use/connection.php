521a11e133e24f43b1ad7131632a32ea13fbda51
jj356 <41375860+jj356@users.noreply.github.com>
2020-07-05 23:16:47 -0400

    Update connection.php

--- Use/connection.php
+++ Use/connection.php
@@ -1,18 +1,9 @@
-<?php
-
-    //Connects to database
-    function dbConnect(){
-        
-      $mydb = new mysqli('127.0.0.1','it490admin','LetM3in!','SportWatch');
-      
-      if ($mydb->errno != 0)
-      {
-      	echo "failed to connect to database: ". $mydb->error . PHP_EOL;
-      	exit(0);
-      }
-        
-      echo "Connection established to database".PHP_EOL;
-      return $mydb;
-}
-
 ?>
+<?php
+$servername = "127.0.0.1";
+$username = "it490admin";
+$password = "LetM3in!";
+$dbname = "SportWatch";
+$con = mysqli_connect($servername,$username,$password,$dbname);
+?>
+
