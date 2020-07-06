2f20b9d65d541a31bf0f03e38ef75cbfb5a33977
dz99NJIT <60232027+dz99NJIT@users.noreply.github.com>
2020-07-06 17:42:03 -0400

    Update landingPage.php

--- APP/landingPage.php
+++ APP/landingPage.php
@@ -25,7 +25,8 @@ session_start();
     	</li>
   	</ul>
   	<ul class="navbar-nav ml-auto">
-   		<form class="form-inline" action="teamSearch.php">
+   		<form class="form-inline" action="cases.php" method="POST">
+		<input type="hidden" id="type" name="type" value="Search">
      		<input class="form-control mr-sm-2" type="text" placeholder="Search for Teams">
      		<button class="btn btn-success" type="submit">Search</button>
    		</form>
