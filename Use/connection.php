4b0a25cbb7669db0ac49d136a9f74d8ea2ba07b9
ELVIS A RODRIGUEZ <40173726+elvisr21@users.noreply.github.com>
2020-07-05 23:25:17 -0400

    commit

--- Use/connection.php
+++ Use/connection.php
@@ -1,8 +1,7 @@
 <?php
-$servername = "127.0.0.1";
+$servername = "127.0.0.1:3306";
 $username = "it490admin";
 $password = "LetM3in!";
 $dbname = "SportWatch";
 $con = mysqli_connect($servername,$username,$password,$dbname);
 ?>
-
