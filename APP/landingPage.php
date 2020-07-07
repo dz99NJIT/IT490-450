497ede62c440bed9cf55a8fdac090349c7a238e2
ELVIS A RODRIGUEZ <40173726+elvisr21@users.noreply.github.com>
2020-07-07 18:34:14 -0400

    Update landingPage.php
    commit

--- APP/landingPage.php
+++ APP/landingPage.php
@@ -38,23 +38,22 @@ session_start();
 						</ul>
 				</nav>
 				<br><br><br>
-		</div>
-		<div class="container-fluid">
-			<?php $uname = $_SESSION["uname"]; echo "<h1>Welcome $uname</h1>"; ?>
+
+				<div class="container-fluid">
+					 <?php $uname = $_SESSION["uname"]; echo "<h1>Welcome $uname</h1>"; ?>
+
+				</div>
 		</div>
 		<div id="teamsearch">
-			<h2>Teams</h2>
 				<form method="POST">
 					<input type="hidden" id="type" name="type" value="Search">
 					<input type="text" id="searchText" placeholder="Search for a Team" name="searchText"><br><br>
 					<input type="button" onclick="teamsearch()" class="btn btn-dark" value="Search"><br><br><br>
 				</form>
+				<div id="teamresult"></div>
 		</div>
-		<div id="posts"> 
-			<h2>News Feed</h2>
-		</div>
+		<div id="posts"> make a post first</div>
 		<div id="sport_feed">
-			<h2>Twitter Feed</h2>
 				<button type="button" value= "espn" onclick="buttonclick(this)">ESPN</button>
 				<button type="button" value= "lol" onclick="buttonclick(this)">League Of Legends</button>
 				<button type="button" value="csgo"onclick="buttonclick(this)">Counter-Strike: Global Offensive</button>
