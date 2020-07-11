5cec0e62dc52114bbf8f0183a438ad7e4d89a9df
ELVIS A RODRIGUEZ <40173726+elvisr21@users.noreply.github.com>
2020-07-11 16:29:47 -0400

    commit

--- DB/dbFunctions.php
+++ DB/dbFunctions.php
@@ -1,22 +1,22 @@
-<?php 
+<?php
 
     require_once('path.inc');
     require_once('get_host_info.inc');
     require_once('rabbitMQLib.inc');
     require_once('AppRabbitMQClient.php');
     require_once('dbConnect.php');
-    
+
     //Login function
     function doLogin($uname, $pw){
-        
+
         $mydb = dbConnect();
-	
+
         $query = "select pw from users where username = '$uname';";
 	$response = $mydb->query($query);
-	 
+
  	$numRows = mysqli_num_rows($response);
 	$responseArray = $response -> fetch_assoc();
-	
+
         if ($numRows>0)
 	{
 		if(password_verify($pw, $responseArray['pw']))
@@ -40,23 +40,23 @@
         $pw = password_hash($pw, PASSWORD_DEFAULT);
 	$key = md5(time().$uname);
         $mydb = dbConnect();
-	 
+
 	$query = "INSERT INTO `users`(`fullname`, `email`, `username`, `pw`, `verificationkey`) VALUES ('$Fullname','$email','$uname','$pw', '$key');";
         //$query = "insert into users values ('$Fullname', '$uname', '$pw');";
         $response = mysqli_query($mydb, $query);
-	    
+
     	return true;
     }
 
     function search($searchText){
-        
+
         $mydb = dbConnect();
-	
+
         $query = "SELECT Players.Name AS 'Player_Name', Teams.Name, Teams.Sport FROM Players INNER JOIN Teams ON Players.Team_ID = Teams.ID WHERE Teams.Name = '$searchText';";
 
 	$response = $mydb->query($query);
  	$numRows = mysqli_num_rows($response);
-	    
+
 	$returnVal = "";
 	$returnVal.="<table border=1px style='width:100%'>";
 	$returnVal.="<tr>";
@@ -64,7 +64,7 @@
      	$returnVal.="<th>Team</th>";
       	$returnVal.="<th>Sport</th>";
       	$returnVal.="</tr>";
-	while($responseArray = $response -> fetch_assoc()){
+	while($responseArray = mysqli_fetch_array($response)){
 	  $returnVal.="<tr>";
           $returnVal.="<td>" . $responseArray[0] . "</td>";
           $returnVal.="<td>" . $responseArray[1] . "</td>";
