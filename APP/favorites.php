634f842b66d31d4a9484e19c439d97492b5b4b94
ELVIS A RODRIGUEZ <40173726+elvisr21@users.noreply.github.com>
2020-07-23 21:38:07 -0400

    commit

--- APP/favorites.php
+++ APP/favorites.php
@@ -10,7 +10,7 @@ session_start();
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-	<script src=landingPage.js></script>
+	<script src="favorite.js"></script>
 	<link rel="stylesheet" href="landingPage.css">
 </head>
 <body>
@@ -39,11 +39,11 @@ session_start();
 								</ul>
 						</nav>
 						<br><br><br>
-            
-            <div>
-            
+
+            <div id="favoriteTeams">
+
             Favorites page
-            
+
             </div>
 		</main>
 </body>
