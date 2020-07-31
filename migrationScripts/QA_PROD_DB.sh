8dbade973001fbd5041c0993c2781bafc571ad27
jj356 <41375860+jj356@users.noreply.github.com>
2020-07-31 01:12:08 -0400

    ADDED CORRECT IP FOR MACHINES

--- migrationScripts/QA_PROD_DB.sh
+++ migrationScripts/QA_PROD_DB.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-#sudo mkdir Backup-$(date +"%d-%m-%Y")
-#sudo cp -r IT490-450/  Backup-$(date +"%d-%m-%Y")
-sudo scp -i Key3.pem -r DEV_QA_API.sh ubuntu@3.210.133.174:~
+sudo mkdir Backup-$(date +"%d-%m-%Y")
+sudo cp -r IT490-450/  Backup-$(date +"%d-%m-%Y")
+sudo scp -i Key1.pem -r DEV_QA_API.sh ubuntu@54.197.169.78:~
