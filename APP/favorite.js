008c5d65fa2f1547cecf5d31a03efa311db77a11
ELVIS A RODRIGUEZ <40173726+elvisr21@users.noreply.github.com>
2020-08-01 15:31:37 -0400

    ss

--- APP/favorite.js
+++ APP/favorite.js
@@ -16,7 +16,7 @@ function deleteFavorite(element){
   var action="delete";
   var teamresult=document.getElementById("teamresult");
   var http=new XMLHttpRequest();
-  http.open("GET","FavoriteTeam.php?username=" + username + "&teamId=" + teamId + "&action=" +action,true);
+  http.open("GET","AddFavoriteTeam.php?username=" + username + "&teamId=" + teamId + "&action=" +action,true);
   http.send();
   show();
 }
